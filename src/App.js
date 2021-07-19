7be9cb049a3b24d31ef358355fd370556c995cf3
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-07-19 19:27:35 +0600

    Setting up firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -6,15 +6,32 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {selectSendMessageIsOpen} from "./features/mailSlice";
-import { PhoneBluetoothSpeakerTwoTone } from '@material-ui/icons';
-import { selectUser } from './features/userSlice';
+import { login, selectUser } from './features/userSlice';
+import Login from './Login';
+import { auth } from './firebase';
 
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+
+  
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        //if the user is logged in
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        })) 
+      }
+    })
+  }, [])
 
   return (
     <Router>
